e490e6ad162c8138b6f94f87142d3c1496b0c414
Ben McKinley <1526042+joyfulflyer@users.noreply.github.com>
2020-07-11 20:47:41 -0700

    models for tiered song

---
+++ models/tierd_song.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+
+from models.base import Base
+
+
+class Tiered_Song(Base):
+    __tablename__ = 'tierd_song'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False)
+    artist = Column(String(128), nullable=False)
+    song_type = Column(String(256), nullable=True)
+
+    def __repr__(self):
+        return "Song: <id=%r, name=%r>" % \
+            (self.id, self.name)
+
+
+SONG_TYPE_BASIC = "basic"  # simple match - name + artist
+SONG_TYPE_ELASTIC = "elastic"  # matched with close enough elastic search
+SONG_TYPE_MANUAL = "manual"  # manually confirmed to be the same song
+# We want basic match songs first then we use those songs and match them to find more 'correct' songs
