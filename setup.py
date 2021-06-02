b4fb228d174a167c8c238398c86889cab92ecec7
joyfulflyer <1526042+joyfulflyer@users.noreply.github.com>
2021-06-01 19:55:00 -0700

    update dependencies and remove a bunch of unused

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 from os import path
 
 setup(name="billboard-song-converter",
-      version="0.0.1",
+      version="0.0.2",
       description="Convert entries to songs",
       author="Joyfulflyer",
       packages=find_packages(),
       python_requires='<=3.6, <4',
-      install_requires=['Flask', 'Flask-SQLAlchemy', 'PyMySQL', 'SQLAlchemy'])
+      install_requires=['PyMySQL', 'SQLAlchemy'])
